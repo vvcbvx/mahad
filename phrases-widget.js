6892ef01edd2c5aaaa5720a8e89ddfdae2c4249e
vvcbvx <hythmghnam32@gmail.com>
2025-04-16 18:53:02 +0300

    Update phrases-widget.js

--- phrases-widget.js
+++ phrases-widget.js
@@ -97,7 +97,7 @@ class PhrasesWidget {
         /* نافذة العبارات */
         .phrases-widget-overlay {
           position: fixed;
-          top: 220px;
+         bottom : 90px;
           left: 0;
           width: 100%;
           height: 490px;
